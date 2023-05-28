f0302d197ea4490c25c7f3d56d22b4f5863c2d45
ZanderBez <221205@virtualwindow.co.za>
2023-05-28 19:21:42 +0200

    Slides
    Fix Slides

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ addStudent = () => {
         });
 
 
-        alert("Welcome " + nameSurname + "! Thank you for your interest in  " + subject + "! We will contact you via email provided:  " + email + " or alternatively on the phone number: " + number);
+        alert("Welcome " + nameSurname + "! Thank you for your interest in " + subject + "! We will contact you via email provided:  " + email + " or alternatively on the phone number: " + number);
     } else {
         alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested " + nameSurname + "! If you want to sign up, you better CHECK that newsletter box!");
     }
